c302943b78573c1eac0827030f4e54c42b658045
The Briant Carrillo <brianttd24.7@gmail.com>
2023-06-25 11:11:38 -0400

    comenzamos dashboard

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+import Header from '../components/Header.jsx'
 
 export default function Home() {
   return (
@@ -14,8 +12,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className=''>
-        
+      <main className='bg-gray-100 min min-h-screen'>
+        <Header />
       </main>
     </>
   )
